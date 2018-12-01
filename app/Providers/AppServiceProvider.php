eb1a43cd5951a99a72e169451feaa9329398e981
ayoshoks <ayoshokz@gmail.com>
2018-12-01 17:00:43 +0100

    edit hero text

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
- use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaultStringLength('191');
+        // Schema::defaultStringLength('191');
     }
 
     /**
